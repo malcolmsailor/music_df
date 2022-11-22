11337b98d8e1ff3d80bad3b95081ddaa4516c93d
Malcolm Sailor <malcolm.sailor@gmail.com>
2022-11-22 09:33:12 -0500

    two small tweaks

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,4 +4,4 @@ import os
 
 @pytest.fixture()
 def n_kern_files(monkeypatch):
-    monkeypatch.setenv("N_KERN_FILES", os.environ.get("N_KERN_FILES", "10"))
+    monkeypatch.setenv("N_KERN_FILES", os.environ.get("N_KERN_FILES", "1"))
