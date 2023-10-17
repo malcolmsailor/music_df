2b77964cbb5d17bdc36ec4a57b79f1f8ab49cf17
Malcolm Sailor <malcolm.sailor@gmail.com>
2023-10-17 11:21:15 -0400

    add xml_to_csvs script

---
+++ scripts/quantize_with_mscore.py
@@ -0,0 +1,98 @@
+import glob
+import os
+import subprocess
+from dataclasses import dataclass
+from functools import partial
+from multiprocessing import Manager, Pool
+from pathlib import Path
+
+# Musescore appears to trim this amount from the duration of every note, so we
+#   add it back in.
+# TODO: (Malcolm 2023-10-17) do we want to do some sort of rounding afterwards?
+RELEASE_DELTA = 0.00208333000000005
+
+DATASETS = os.environ.get("DATASETS", os.path.expanduser("~/datasets"))
+
+
+@dataclass
+class Config:
+    input_folder: str = os.path.join(DATASETS, "/Users/malcolm/datasets/YCAC-1.0")
+    output_folder: str = os.path.join(
+        DATASETS, "/Users/malcolm/datasets/YCAC-1.0-quantized"
+    )
+    num_workers: int = 16
+    max_files: int | None = None
+    overwrite: bool = False
+
+
+def musescore_quantize(input_path: str, output_path: str):
+    command = ["mscore", input_path, "-o", output_path]
+    subprocess.run(command, check=True, capture_output=True)
+
+
+def get_paths(config):
+    input_and_output_files = []
+
+    for dirpath, dirnames, filenames in os.walk(config.input_folder):
+        relpath = os.path.relpath(dirpath, config.input_folder)
+        for filename in filenames:
+            if filename.endswith(".mid"):
+                input_path = os.path.join(dirpath, filename)
+                output_path = os.path.join(
+                    config.output_folder, relpath, filename[:-4] + ".xml"
+                )
+                input_and_output_files.append((input_path, output_path))
+
+    return input_and_output_files
+
+
+def do_file(paths: tuple[str, str], config, exceptions_list):
+    input_file, output_file = paths
+    if not config.overwrite and os.path.exists(output_file):
+        return
+    print(f"Saving {output_file}")
+    try:
+        musescore_quantize(input_file, output_file)
+    except Exception as exc:
+        exceptions_list.append(f"{input_file}: {repr(exc)}")
+
+
+def make_output_folders(paths):
+    output_folders = {os.path.dirname(f) for _, f in paths}
+    for f in output_folders:
+        os.makedirs(f, exist_ok=True)
+
+
+def run(config: Config):
+    input_and_output_files = get_paths(config)
+    if config.max_files is not None and config.max_files > 0:
+        input_and_output_files = input_and_output_files[: config.max_files]
+    # I'm not sure if multiprocessing will cause any issues if two processes try to
+    #   create a folder at the same time, so we just create all the output folders
+    #   first in the main process.
+    make_output_folders(input_and_output_files)
+
+    if config.num_workers > 1:
+        manager = Manager()
+        exceptions = manager.list()
+        with Pool(config.num_workers) as pool:
+            list(
+                pool.imap_unordered(
+                    partial(do_file, config=config, exceptions_list=exceptions),
+                    input_and_output_files,
+                )
+            )
+    else:
+        exceptions = []
+        for paths in input_and_output_files:
+            do_file(paths, config, exceptions)
+
+    for exception in exceptions:
+        print(exception)
+    if exceptions:
+        print(f"{len(exceptions)} total errors")
+
+
+if __name__ == "__main__":
+    config = Config()
+    run(config)
--- scripts/xml_to_csvs.py
+++ scripts/xml_to_csvs.py
@@ -6,23 +6,21 @@ import random
 import re
 import sys
 import traceback
-import warnings
 from dataclasses import dataclass, field
 from functools import partial
 from multiprocessing import Manager, Pool
 from typing import Iterable
 
 from music_df.quantize_df import quantize_df
-from music_df.read_midi import read_midi
+from music_df.read_xml import read_xml
 from music_df.salami_slice import salami_slice
 from music_df.script_helpers import read_config_oc
 
-# TODO: (Malcolm 2023-10-14) round releases somehow
 # TODO: (Malcolm 2023-10-14) remove doublings in orchestral scores somehow
 
 
 @dataclass
-class MidiToCSVConfig:
+class XMLToCSVConfig:
     input_folder: str
     output_folder: str
     max_files: int | None = None
@@ -33,57 +31,43 @@ class MidiToCSVConfig:
         default_factory=lambda: {"note", "time_signature", "bar"}
     )
     overwrite: bool = False
-    filter_midi_reading_warnings: bool = True
     num_workers: int = 8
     regex: str | None = None
     debug: bool = False
-    # When working with files output by musescore (which I am using to quantize the
-    #   midi) appears to trim this amount from the duration of every note, so we add it
-    #   back in.
-    release_delta: float = 0.00208333000000005
-    quantize_tpq: int = 96
 
 
-def get_midi_files(folder_path):
+def get_xml_files(folder_path):
     # Use os.path.join to construct the search pattern
-    search_pattern = os.path.join(folder_path, "**", "*.mid")
+    search_pattern = os.path.join(folder_path, "**", "*.xml")
 
     # Use glob.glob with recursive=True to get all matching files
     return glob.glob(search_pattern, recursive=True)
 
 
-def do_midi_file(midi_file, config, output_list, error_file_list):
+def do_xml_file(xml_file, config, output_list, error_file_list):
     try:
         output_basename = (
-            midi_file.replace(config.input_folder, "")
+            xml_file.replace(config.input_folder, "")
             .lstrip(os.path.sep)
             .replace(" ", "_")
             .replace(os.path.sep, "+")
-            .replace(".mid", ".csv")
+            .replace(".xml", ".csv")
         )
         output_path = os.path.join(config.output_folder, output_basename)
 
         if os.path.exists(output_path) and not config.overwrite:
             return
 
-        if config.filter_midi_reading_warnings:
-            warnings.filterwarnings("ignore", message="note_off event")
-
-        music_df = read_midi(midi_file)
+        music_df = read_xml(xml_file)
         music_df = music_df[music_df.type.isin(config.event_types)]
-        music_df = music_df.drop("filename", axis=1)
-        if config.release_delta:
-            music_df["release"] = music_df["release"] + config.release_delta
-        if config.quantize_tpq:
-            music_df = quantize_df(music_df, tpq=config.quantize_tpq)
         if config.salami_slice:
             music_df = salami_slice(music_df)
         music_df.to_csv(output_path)
 
         print(f"Wrote {output_path}")
-        output_list.append(midi_file)
+        output_list.append(xml_file)
     except Exception as exc:
-        error_file_list.append((midi_file, repr(exc)))
+        error_file_list.append((xml_file, repr(exc)))
 
 
 def write_json(config, output_files):
@@ -100,7 +84,7 @@ def write_json(config, output_files):
 
 def main():
     config = read_config_oc(
-        config_path=None, cli_args=sys.argv[1:], config_cls=MidiToCSVConfig
+        config_path=None, cli_args=sys.argv[1:], config_cls=XMLToCSVConfig
     )
     if config.debug:
 
@@ -112,14 +96,14 @@ def main():
 
         sys.excepthook = custom_excepthook
 
-    midi_files = get_midi_files(config.input_folder)
+    xml_files = get_xml_files(config.input_folder)
     if config.regex is not None:
-        midi_files = [f for f in midi_files if re.search(config.regex, f)]
+        xml_files = [f for f in xml_files if re.search(config.regex, f)]
     random.seed(config.seed)
     if config.random_files:
-        random.shuffle(midi_files)
+        random.shuffle(xml_files)
 
-    midi_files = midi_files[: config.max_files]
+    xml_files = xml_files[: config.max_files]
     os.makedirs(config.output_folder, exist_ok=True)
     if config.num_workers > 1:
         manager = Manager()
@@ -129,26 +113,26 @@ def main():
             list(
                 pool.imap_unordered(
                     partial(
-                        do_midi_file,
+                        do_xml_file,
                         config=config,
                         output_list=output_files,
                         error_file_list=error_files,
                     ),
-                    midi_files,
+                    xml_files,
                 )
             )
     else:
         output_files = []
         error_files = []
-        for midi_file in midi_files:
-            do_midi_file(midi_file, config, output_files, error_files)
+        for xml_file in xml_files:
+            do_xml_file(xml_file, config, output_files, error_files)
 
     write_json(config, output_files)
 
     if error_files:
         print("Errors:")
-        for midi_file, exception_str in error_files:
-            print(f"{midi_file}: {exception_str}")
+        for xml_file, exception_str in error_files:
+            print(f"{xml_file}: {exception_str}")
         print(f"{len(error_files)} total error files")
 
 
