d450b8a4e741e193379ad5cb388c3275d4508923
Malcolm Sailor <malcolm.sailor@gmail.com>
2023-10-19 09:53:25 -0400

    fix augmentations issue

--- music_df/augmentations.py
+++ music_df/augmentations.py
@@ -1,5 +1,7 @@
+import math
 import random
 from ast import literal_eval
+from copy import deepcopy
 from typing import Iterable, Iterator
 
 import pandas as pd
@@ -56,17 +58,124 @@ def _to_dict_if_necessary(d):
 
 
 def scale_time_sigs(music_df: pd.DataFrame, factor: int | float) -> pd.DataFrame:
+    """
+    >>> df = pd.DataFrame(
+    ...     {
+    ...         "pitch": [float("nan"), float("nan")],
+    ...         "onset": [0, 16],
+    ...         "type": ["time_signature", "time_signature"],
+    ...         "other": [
+    ...             {"numerator": 4, "denominator": 1},
+    ...             {"numerator": 3, "denominator": 16},
+    ...         ],
+    ...     }
+    ... )
+    >>> df
+       pitch  onset            type                                other
+    0    NaN      0  time_signature   {'numerator': 4, 'denominator': 1}
+    1    NaN     16  time_signature  {'numerator': 3, 'denominator': 16}
+
+    >>> scale_time_sigs(df, 0.5)
+       pitch  onset            type                                other
+    0    NaN      0  time_signature   {'numerator': 4, 'denominator': 2}
+    1    NaN     16  time_signature  {'numerator': 3, 'denominator': 32}
+
+
+    If denominator would be fractional, we scale up the numerator (this won't always
+    give totally correct results, e.g., 3/1 -> 6/1 with doubled note values which has
+    different metric implications.)
+
+    >>> scale_time_sigs(df, 2.0)
+       pitch  onset            type                               other
+    0    NaN      0  time_signature  {'numerator': 8, 'denominator': 1}
+    1    NaN     16  time_signature  {'numerator': 3, 'denominator': 8}
+
+    >>> scale_time_sigs(df, 0.2)
+    Traceback (most recent call last):
+    AssertionError: factor=0.2 must be a power of 2
+    """
+    assert not math.log2(factor) % 1, f"{factor=} must be a power of 2"
     time_sig_mask = music_df.type == "time_signature"
     if not time_sig_mask.any():
         return music_df
+    music_df = music_df.copy()
     time_sigs = music_df[music_df.type == "time_signature"]
     time_sigs.loc[:, "other"] = time_sigs["other"].apply(_to_dict_if_necessary)
+    updated_time_sigs = []
     for _, time_sig in time_sigs.iterrows():
-        new_denominator = factor * time_sig["other"]["denominator"]
+        numerator = time_sig["other"]["numerator"]
+        # The denominator should get bigger when the notes get shorter
+        #   and vice versa
+        new_denominator = time_sig["other"]["denominator"] / factor
+        while new_denominator % 1:
+            numerator *= 2
+            new_denominator *= 2
         assert not new_denominator % 1
-        time_sig["other"]["denominator"] = int(new_denominator)
-    music_df.loc[time_sig_mask] = time_sigs
+        new_dict = deepcopy(time_sig["other"])
+        new_dict["denominator"] = int(new_denominator)
+        new_dict["numerator"] = int(numerator)
+        time_sig["other"] = new_dict
+        updated_time_sigs.append(time_sig)
+    music_df.loc[time_sig_mask] = updated_time_sigs
     return music_df
+
+
+def scale_df(df: pd.DataFrame, factor: float, metadata: bool = True) -> pd.DataFrame:
+    """
+    >>> nan = float("nan")  # Alias to simplify below assignments
+    >>> df = pd.DataFrame(
+    ...     {
+    ...         "pitch": [nan, 60, nan, 64],
+    ...         "onset": [0, 0, 16, 16.5],
+    ...         "release": [nan, 4, nan, 17],
+    ...         "type": ["time_signature", "note", "time_signature", "note"],
+    ...         "other": [
+    ...             {"numerator": 4, "denominator": 1},
+    ...             nan,
+    ...             {"numerator": 3, "denominator": 16},
+    ...             nan,
+    ...         ],
+    ...     }
+    ... )
+    >>> df
+       pitch  onset  release            type                                other
+    0    NaN    0.0      NaN  time_signature   {'numerator': 4, 'denominator': 1}
+    1   60.0    0.0      4.0            note                                  NaN
+    2    NaN   16.0      NaN  time_signature  {'numerator': 3, 'denominator': 16}
+    3   64.0   16.5     17.0            note                                  NaN
+
+    >>> scale_df(df, 0.5)
+       pitch  onset  release            type                                other
+    0    NaN   0.00      NaN  time_signature   {'numerator': 4, 'denominator': 2}
+    1   60.0   0.00      2.0            note                                  NaN
+    2    NaN   8.00      NaN  time_signature  {'numerator': 3, 'denominator': 32}
+    3   64.0   8.25      8.5            note                                  NaN
+
+    >>> scale_df(df, 2.0)
+       pitch  onset  release            type                               other
+    0    NaN    0.0      NaN  time_signature  {'numerator': 8, 'denominator': 1}
+    1   60.0    0.0      8.0            note                                 NaN
+    2    NaN   32.0      NaN  time_signature  {'numerator': 3, 'denominator': 8}
+    3   64.0   33.0     34.0            note                                 NaN
+
+    >>> scale_df(df, 0.2)
+    Traceback (most recent call last):
+    AssertionError: factor=0.2 must be a power of 2
+
+    """
+    assert not math.log2(factor) % 1, f"{factor=} must be a power of 2"
+    aug_df = df.copy()
+    aug_df["onset"] *= factor
+    aug_df["release"] *= factor
+
+    aug_df = scale_time_sigs(aug_df, factor)
+
+    if metadata:
+        if "rhythms_scaled_by" in aug_df.attrs:
+            aug_df.attrs["rhythms_scaled_by"] *= factor
+        else:
+            aug_df.attrs["rhythms_scaled_by"] = factor
+    return aug_df
 
 
 def aug_rhythms(
@@ -109,18 +218,19 @@ def aug_rhythms(
                 yield df
             else:
                 scale_factor = 2.0**pow_of_2
+                yield scale_df(df, scale_factor, metadata=metadata)
 
-                aug_df = df.copy()
-                aug_df["onset"] *= scale_factor
-                aug_df["release"] *= scale_factor
+                # aug_df = df.copy()
+                # aug_df["onset"] *= scale_factor
+                # aug_df["release"] *= scale_factor
 
-                # The denominator should get bigger when the notes get shorter
-                #   and vice versa
-                scale_time_sigs(aug_df, 1 / scale_factor)
+                # # The denominator should get bigger when the notes get shorter
+                # #   and vice versa
+                # scale_time_sigs(aug_df, 1 / scale_factor)
 
-                if metadata:
-                    if "rhythms_scaled_by" in aug_df.attrs:
-                        aug_df.attrs["rhythms_scaled_by"] *= scale_factor
-                    else:
-                        aug_df.attrs["rhythms_scaled_by"] = scale_factor
-                yield aug_df
+                # if metadata:
+                #     if "rhythms_scaled_by" in aug_df.attrs:
+                #         aug_df.attrs["rhythms_scaled_by"] *= scale_factor
+                #     else:
+                #         aug_df.attrs["rhythms_scaled_by"] = scale_factor
+                # yield aug_df
